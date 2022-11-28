6913059c63797f484cd79296a2d62f68184d6d43
mhmdrizky26 <mhmdrizky2605@gmail.com>
2022-11-28 14:57:47 +0700

    3

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,7 +5,7 @@
     <section class="section">
         @hasanyrole('admin')
         <div class="section-header">
-          <h1>Dashboard</h1>
+          <h1>Menu Utama</h1>
         </div>
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
@@ -34,21 +34,6 @@
                   </div>
                   <div class="card-body">
                     {{ App\Models\Question::count() ?? '0' }}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-              <div class="card card-statistic-1">
-                <div class="card-icon bg-warning">
-                  <i class="fa fa-atlas text-white fa-2x"></i>
-                </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>SUBJECTS</h4>
-                  </div>
-                  <div class="card-body">
-                    {{ App\Models\Subject::count() ?? '0' }}
                   </div>
                 </div>
               </div>
