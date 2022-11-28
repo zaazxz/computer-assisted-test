a9cf98e42df685c0096045733c947e3ef2b631aa
zaazxz <mirzaqamaruzzaman18@gmail.com>
2022-11-28 15:02:18 +0700

    kuli

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,7 +5,7 @@
     <section class="section">
         @hasanyrole('admin')
         <div class="section-header">
-          <h1>Menu Utama</h1>
+          <h1>Dashboard</h1>
         </div>
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
